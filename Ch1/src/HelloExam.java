90e9b5b8a2dcf8f0d4f1ce68e58d2be2b6884955
Josef Koza <josef.a.koza@gmail.com>
2013-12-16 13:04:20 -0500

    Complete samples and exercises through Ch 2.1

---
+++ Ch1/src/HelloExam.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Josef on 12/13/13.
+ */
+public class HelloExam {
+    public static void main(String[] args) {
+        System.out.println("Hello dude");
+    }
+}
---
+++ Ch2/src/TwistInTaleNumberSystem.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Josef on 12/16/13.
+ */
+class TwistInTaleNumberSystem {
+    public static void main (String args[]) {
+        int baseDecimal = 267;
+        int octVal = 0413;
+        int hexVal = 0x10B;
+        int binVal = 0b100001011;
+        System.out.println(baseDecimal + octVal);
+        System.out.println(hexVal + binVal);
+    }
+}
