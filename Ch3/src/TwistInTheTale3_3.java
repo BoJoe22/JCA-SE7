601856924e1fc4146c6b2947cd496562b14579d5
bojoe22 <josef.a.koza@gmail.com>
2013-12-27 11:49:55 -0500

    Add code for Twist in the Tale 3.3

---
+++ Ch3/src/TwistInTheTale3_3.java
@@ -0,0 +1,10 @@
+/**
+ * Twist in the Tale 3.3 from the OCA Java SE 7 book by Mala Gupta (Manning Publications)
+ * Created by Josef on 12/27/13.
+ */
+class Phone2 {
+    public String model;
+    private double weight;
+    public void setWeight(double w) {weight = w;}
+    public double getWeight() {return weight;}
+}
